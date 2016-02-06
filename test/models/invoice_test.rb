9696d08fea7727f9de9773897225df55b7a2089a
agudeluca <agustin_deluca@hotmail.com>
2016-02-06 07:46:11 -0300

    arregle los tests

--- test/models/invoice_test.rb
+++ test/models/invoice_test.rb
@@ -5,7 +5,6 @@ class CustomerTest < ActiveSupport::TestCase
 	test  "Con algun dato no valido(dni <> 8)" do
 	     	a=Customer.new(name:'sadsad',last_name:'saffgdg',genre:'Male',birthdate:Date.today,cuil_cuit:'23-36512477-9',document_number:32323236058018)
             a.invoices.new(details:"compra de casa",amount:0.3,date:Date.today,reveiver:"12324523451",cuil_cuit_reveiver:'23-36512465-9')
-            puts a.invoices.first.cuil_cuit
             a.contacts.new(type_contact:'email',contact_value:'adsadas@sadsa')
             assert_not a.save,"Se guardo un cliente con datos de contacto"  
 	     end
@@ -13,7 +12,6 @@ class CustomerTest < ActiveSupport::TestCase
 	     test "Con alguna factura valida" do
             a=Customer.new(name:'sadsad',last_name:'saffgdg',genre:'Male',birthdate:Date.today,cuil_cuit:'23-36512477-9',document_number:32322323)
             a.invoices.new(details:"compra de casa",amount:0.3,date:Date.today,reveiver:"12324523",cuil_cuit_reveiver:'23-36212469-9')
-            puts a.invoices.first.cuil_cuit
             a.contacts.new(type_contact:'email',contact_value:'adsadas@sadsa')
             assert a.save,"Se guardo un cliente con una factura valida" 
 	     end
