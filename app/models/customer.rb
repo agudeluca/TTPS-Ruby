34d0d2b2cf5faf7575620f52df08e3b738bd4606
agudeluca <agustin_deluca@hotmail.com>
2016-02-08 18:04:04 -0300

    arregle el issue Uso De Framework

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -30,13 +30,21 @@ class Customer < ActiveRecord::Base
 	accepts_nested_attributes_for :contacts
 
 
-
+def update_contacts(datatoupdate)
+    guardo=false
+    datatoupdate.each do |dato|
+    contact = Contact.find_by_id(dato[0])
+    guardo = contact.update(type_contact: dato[1]['type_contact'].to_s,contact_value: dato[1]['contact_value'].to_s)
+  end
+  return guardo
+end
+=begin
 	def update_contacts(datatoupdate)
 	      datatoupdate_enum=datatoupdate.to_enum
           contacts_enum=contacts.all.to_enum
           inserto=false
         loop do
-
+            halt
             aux=datatoupdate_enum.next
             inserto=contacts_enum.next.update(type_contact: aux[1]['type_contact'].to_s,
             	                              contact_value: aux[1]['contact_value'].to_s)
@@ -46,16 +54,19 @@ class Customer < ActiveRecord::Base
         end
         return inserto
     end
-
+=end
 
         
  	def update_values(customer,contacts,new_contact)
         @new=Contact.new(type_contact: new_contact["type_contact"],contact_value:new_contact["contact_value"])
     	if !@new.valid?
-    	(update_attributes(customer) &&  update_contacts(contacts))
+    	(update_attributes(customer) &&  
+        update_contacts(contacts))
     	 
     	else
-    	(update_attributes(customer) && update_contacts(contacts) && self.contacts.new(type_contact: new_contact["type_contact"],contact_value:new_contact["contact_value"]).save)
+    	(update_attributes(customer) && 
+        update_contacts(contacts) && 
+        self.contacts.new(type_contact: new_contact["type_contact"],contact_value:new_contact["contact_value"]).save)
         
         end
 	    end
@@ -64,7 +75,8 @@ class Customer < ActiveRecord::Base
 
 	def calcular_edad
   		now = Time.now.utc.to_date
-  		return now.year - self.birthdate.year - ((now.month > self.birthdate.month || (now.month == self.birthdate.month && now.day >= self.birthdate.day)) ? 0 : 1)
+  		return now.year - self.birthdate.year - 
+      ((now.month > self.birthdate.month || (now.month == self.birthdate.month && now.day >= self.birthdate.day)) ? 0 : 1)
 	end
 
 
@@ -78,7 +90,8 @@ class Customer < ActiveRecord::Base
 
 
     def monto_total_por_mes
-    	@ar=[["Enero", 0],[ "Febrero", 0],[ "Marzo", 0],["Abril", 0],["Mayo", 0],["Junio", 0],["Julio", 0],["Agosto", 0],["Septiembre", 0],["Octubre", 0],[ "Noviembre", 0],["Diciembre",0]]
+    	@ar=[["Enero", 0],[ "Febrero", 0],[ "Marzo", 0],["Abril", 0],["Mayo", 0],
+      ["Junio", 0],["Julio", 0],["Agosto", 0],["Septiembre", 0],["Octubre", 0],[ "Noviembre", 0],["Diciembre",0]]
     	@facturas=self.invoices.select { |num|  num.date.year == Date.today.year}
         @facturas.each do |fac|
            @ar[fac.date.month-1][1]+=1
